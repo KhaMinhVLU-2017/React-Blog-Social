814e7e853d9000cccf075e902154dfdae311f32a
minh <minhnguyengit@gmail.com>
2018-10-05 15:27:35 +0700

    Fix error Console Props pass params from Route to Component

--- Client/src/index.js
+++ Client/src/index.js
@@ -100,7 +100,7 @@ class RouteApp extends Component {
                     // history.push('/Login')
                     return <Signup />
                   }} />
-                  <Route path='/article/:idPost' component={SingleArticle} />
+                  <Route path='/article/:idPost' render={(props) => <SingleArticle {...props} />} />
                   <Route path='/articles/create' render={() => {
                     let { cookies } = this.props
                     let history = createHistory()
